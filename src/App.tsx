0f6b995113d21921aea026b3cf775905c372d0f4
Ross Tungol <ross@proudcloud.io>
2023-11-06 20:58:03 +0800

    Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <h1>Notes</h1>
+      <Routes>
+        <Route path='/' element={<h1>Notes</h1>} />
+        <Route path='/new' element={<h1>New</h1>} />
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
     </>
   )
 }
