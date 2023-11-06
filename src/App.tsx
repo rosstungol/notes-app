5e2885aa75f43334e2acc25a2ca6a9877b37f82c
Ross Tungol <ross@proudcloud.io>
2023-11-06 21:00:14 +0800

    Adds Container component from react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
   return (
-    <>
+    <Container className='my-4'>
       <Routes>
         <Route path='/' element={<h1>Notes</h1>} />
         <Route path='/new' element={<h1>New</h1>} />
@@ -13,7 +14,7 @@ function App() {
         </Route>
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
-    </>
+    </Container>
   )
 }
 
