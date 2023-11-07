15f573dc0069dc61db187ab922eb65fe88bc188e
Ross Tungol <ross@proudcloud.io>
2023-11-07 18:25:32 +0800

    Adds type for RawNote

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ export type Note = {
   id: string
 } & NoteData
 
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
 export type NoteData = {
   title: string
   markdown: string
