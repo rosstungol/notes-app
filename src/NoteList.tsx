548381eb41668a3a432b4516b7b54c26246c3408
Ross Tungol <ross@proudcloud.io>
2023-11-09 21:00:13 +0800

    Adds NoteCard component

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -65,3 +65,35 @@ export function NoteList({ availableTags }: NoteListProps) {
     </>
   )
 }
+
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body>
+        <Stack
+          gap={2}
+          className='align-items-center justify-content-center h-100'
+        >
+          <span className='fs-5'>{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction='horizontal'
+              className='justify-content-center flex-wrap'
+            >
+              {tags.map((tag) => (
+                <Badge key={tag.id} className='text-truncate'>
+                  {tag.label}
+                </Badge>
+              ))}{" "}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
+  )
+}
