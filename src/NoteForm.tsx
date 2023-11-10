5c8ce57fe892add1323a8658507b7c51720e32ed
Ross Tungol <ross@proudcloud.io>
2023-11-10 14:34:37 +0800

    Adds default value to title and textarea input when editing note

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -43,7 +43,7 @@ export function NoteForm({
           <Col>
             <Form.Group controlId='title'>
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} defaultValue={title} required />
             </Form.Group>
           </Col>
           <Col>
@@ -75,7 +75,13 @@ export function NoteForm({
         </Row>
         <Form.Group controlId='markdown'>
           <Form.Label>Body</Form.Label>
-          <Form.Control ref={markdownRef} required as='textarea' rows={15} />
+          <Form.Control
+            ref={markdownRef}
+            defaultValue={markdown}
+            required
+            as='textarea'
+            rows={15}
+          />
         </Form.Group>
         <Stack direction='horizontal' gap={2} className='justify-content-end'>
           <Button type='submit' variant='primary'>
