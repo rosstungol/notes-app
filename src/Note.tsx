9ea211e1675306d272a82b0e8b8988442e5a35ea
Ross Tungol <ross@proudcloud.io>
2023-11-10 14:09:13 +0800

    Adds ReactMarkdown to Note component

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,6 +1,7 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap"
 import { useNote } from "./NoteLayout"
 import { Link } from "react-router-dom"
+import ReactMarkdown from "react-markdown"
 
 export function Note() {
   const note = useNote()
@@ -32,6 +33,7 @@ export function Note() {
           </Stack>
         </Col>
       </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
   )
 }
