0a56f83bb578d86018bbde8605594afea849abb8
Ross Tungol <ross@proudcloud.io>
2023-11-10 13:25:05 +0800

    Adds Note component

---
+++ src/Note.tsx
@@ -0,0 +1,25 @@
+import { Badge, Col, Row, Stack } from "react-bootstrap"
+import { useNote } from "./NoteLayout"
+
+export function Note() {
+  const note = useNote()
+
+  return (
+    <>
+      <Row className='align-items-center mb-4'>
+        <Col>
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction='horizontal' className='flex-wrap'>
+              {note.tags.map((tag) => (
+                <Badge key={tag.id} className='text-truncate'>
+                  {tag.label}
+                </Badge>
+              ))}{" "}
+            </Stack>
+          )}
+        </Col>
+      </Row>
+    </>
+  )
+}
