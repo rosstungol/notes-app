ffc9e3bfff9b6f47c05374109bce84b1499d8597
Ross Tungol <ross@proudcloud.io>
2023-11-17 14:34:24 +0800

    Adds conditional rendering for empty tags

--- src/components/EditTagsModal.tsx
+++ src/components/EditTagsModal.tsx
@@ -16,34 +16,41 @@ export function EditTagsModal({
   onUpdateTag,
   onDeleteTag
 }: EditTagsModalProps) {
+  const modalBody =
+    availableTags.length > 0 ? (
+      availableTags.map((tag) => {
+        return (
+          <Row key={tag.id}>
+            <Col>
+              <Form.Control
+                type='text'
+                value={tag.label}
+                onChange={(e) => onUpdateTag(tag.id, e.target.value)}
+              />
+            </Col>
+            <Col xs='auto'>
+              <Button
+                variant='outline-danger'
+                onClick={() => onDeleteTag(tag.id)}
+              >
+                &times;
+              </Button>
+            </Col>
+          </Row>
+        )
+      })
+    ) : (
+      <span>No tags yet.</span>
+    )
+
   return (
     <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>Edit Tags</Modal.Header>
+      <Modal.Header closeButton>
+        <h5 className='my-1'>Edit Tags</h5>
+      </Modal.Header>
       <Modal.Body>
         <Form>
-          <Stack gap={2}>
-            {availableTags.map((tag) => {
-              return (
-                <Row key={tag.id}>
-                  <Col>
-                    <Form.Control
-                      type='text'
-                      value={tag.label}
-                      onChange={(e) => onUpdateTag(tag.id, e.target.value)}
-                    />
-                  </Col>
-                  <Col xs='auto'>
-                    <Button
-                      variant='outline-danger'
-                      onClick={() => onDeleteTag(tag.id)}
-                    >
-                      &times;
-                    </Button>
-                  </Col>
-                </Row>
-              )
-            })}
-          </Stack>
+          <Stack gap={2}>{modalBody}</Stack>
         </Form>
       </Modal.Body>
     </Modal>
